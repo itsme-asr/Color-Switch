c29e8ff5f7fe93000a4dd0786591d37e290be60a
A.S.Rawat <adityasrwt5@gmail.com>
2022-12-02 21:22:43 +0530

    Color Switch

--- Color Switch/Assets/Script/Player.cs
+++ Color Switch/Assets/Script/Player.cs
@@ -1,17 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float jumpForce = 8f;
+    public float jumpForce = 7f;
     public Rigidbody2D rb;
-    void Update()
+    public string currentColor;
+    public SpriteRenderer spr;
+    public Color colorCyan;
+    public Color colorYellow;
+    public Color colorPink;
+    public Color colorViolet;
+
+    private void Start()
+    {
+        setRandomColor();
+    }
+    private void Update()
     {
         if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(0))
         {
             rb.velocity = Vector2.up * jumpForce;
         }
 
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag != currentColor)
+        {
+            // pass or destory
+            Debug.Log("GAME OVER");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (col.tag == "colorChange")
+        {
+            setRandomColor();
+            Destroy(col.gameObject);
+            return;
+        }
+
+    }
+
+    void setRandomColor()
+    {
+        int index = Random.Range(0, 4);
+        switch (index)
+        {
+            case 0:
+                currentColor = "Cyan";
+                spr.color = colorCyan;
+                break;
+            case 1:
+                currentColor = "Yellow";
+                spr.color = colorYellow;
+                break;
+            case 2:
+                currentColor = "Pink";
+                spr.color = colorPink;
+                break;
+            case 3:
+                currentColor = "Violet";
+                spr.color = colorViolet;
+                break;
+        }
     }
 }
